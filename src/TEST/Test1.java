5c9d7f4efcf19836ed6c9b9f266bbf33a1768a9b
MishaTur <turokmssha8888@gmail.com>
2021-11-09 13:38:27 +0200

    ADD TEST

---
+++ src/TEST/Test1.java
@@ -0,0 +1,8 @@
+package TEST;
+
+public class Test1 {
+    static public void main(String[] args){
+        System.out.println("Hi Saha");
+        System.out.println("TEST1.2");
+    }
+}
---
+++ src/TEST/Test3.java
@@ -0,0 +1,7 @@
+package TEST;
+
+public class Test3 {
+    static public void main(String[] args){
+        System.out.println("Hi Sahaha");
+        System.out.println("TEST1.2");}
+}
